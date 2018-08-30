e9f56a367ddf45c8a4af6fdd539638e10d4bbd45
Paul Vladimirov <dr.scroodge@gmail.com>
2018-08-31 00:36:11 +0300

    json beautifier

---
+++ JsonBeautifier/JsonBeautifier/Program.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+
+namespace JsonBeautifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootDir = Directory.GetCurrentDirectory();
+
+            rootDir = @"d:\Repositories\msf\Config\";
+
+            Console.WriteLine("parsing dir: {0}", rootDir);
+
+            ProcessDirectory(rootDir);
+
+            Console.ReadKey();
+        }
+
+        static void ProcessDirectory(string path)
+        {
+            foreach (string file in Directory.GetFiles(path))
+            {
+                if (file.EndsWith(".json"))
+                {
+                    string json1 = File.ReadAllText(file);
+
+                    string json2 = JToken.Parse(json1).ToString(Formatting.Indented);
+
+                    Console.WriteLine("file: {0} --- {1} => {2}", file, json1.Length, json2.Length);
+
+                    File.WriteAllText(file, json2);
+                }
+            }
+
+            foreach (string subDir in Directory.GetDirectories(path))
+            {
+                ProcessDirectory(subDir);
+            }
+        }
+    }
+}
