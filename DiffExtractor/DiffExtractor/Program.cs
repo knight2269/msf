b0cf0761a328fde8e70cc314ca8547d9b10a8175
Paul Vladimirov <dr.scroodge@gmail.com>
2018-09-06 02:53:17 +0300

    diff extractor upd

--- DiffExtractor/DiffExtractor/Program.cs
+++ DiffExtractor/DiffExtractor/Program.cs
@@ -11,6 +11,7 @@ namespace DiffExtractor
             if (args == null || args.Length != 2)
             {
                 Console.WriteLine("usage: [exe name] [file name 1] [file name 2]");
+                Console.ReadKey();
                 return;
             }
 
@@ -19,25 +20,50 @@ namespace DiffExtractor
                 if (File.Exists(args[i]) == false)
                 {
                     Console.WriteLine("file {0} not found", args[i]);
+                    Console.ReadKey();
+                    return;
                 }
             }
 
             List<string> lines0 = new List<string>(File.ReadAllLines(args[0]));
             List<string> lines1 = new List<string>(File.ReadAllLines(args[1]));
 
+            int _checked = 0;
+            int _removed = 0;
+
             for (int indexFrom1 = lines1.Count - 1; indexFrom1 >= 0; indexFrom1--)
             {
-                int indexFrom0 = lines0.IndexOf(lines1[indexFrom1]);
+                int indexFrom0 = lines0.FindIndex(x => AreSame(x, lines1[indexFrom1]));
 
+                _checked++;
                 if (indexFrom0 >= 0)
                 {
                     lines0.RemoveAt(indexFrom0);
                     lines1.RemoveAt(indexFrom1);
+                    _removed++;
                 }
             }
 
             File.WriteAllLines(args[0], lines0);
             File.WriteAllLines(args[1], lines1);
+
+            Console.WriteLine("{0} removed of {1} lines", _removed, _checked);
+            Console.ReadKey();
+        }
+
+        private static bool AreSame(string line1, string line2)
+        {
+            string[] line1Splitted = line1.Split(',', StringSplitOptions.RemoveEmptyEntries);
+            string[] line2Splitted = line2.Split(',', StringSplitOptions.RemoveEmptyEntries);
+
+            if (line1Splitted.Length != line2Splitted.Length) { return false; }
+
+            for (int i = 0; i < line1Splitted.Length; i++)
+            {
+                if (line1Splitted[i] != line2Splitted[i]) { return false; }
+            }
+
+            return true;
         }
     }
 }
